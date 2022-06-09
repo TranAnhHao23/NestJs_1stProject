e060b055d96f66107705d9f827be98c9c899ad3c
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-06-09 13:31:02 +0700

    2nd commit

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './create-post.dto';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postService: PostsService) {}
+
+  @Get()
+  async getPosts() {
+    return await this.postService.getPosts();
+  }
+
+  @Get(':idPost')
+  async getPost(@Param('idPost') idPost) {
+    return await this.postService.getPost(idPost);
+  }
+
+  @Post()
+  async addPost(@Body() createPostDTO: CreatePostDto) {
+    return await this.postService.addPost(createPostDTO);
+  }
+
+  @Delete(':postId')
+  async deletePost(@Param('postId') idPost) {
+    return await this.postService.deletePost(idPost);
+  }
+}
